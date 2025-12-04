1ab96b82db35aa08e8800005598e68cd2ba3df1c
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2025-12-04 18:21:33 +0800

    12_pwm_steering_engine2025.1
    pwm驱动舵机

---
+++ 12_pwm_steering_engine2025.1/Hardware/Servo.c
@@ -0,0 +1,13 @@
+#include "stm32f10x.h"                  // Device header
+#include "PWM.h"
+
+void Servo_Init(void)
+{
+	PWM_Init();
+}
+
+
+void Servo_SetAngle(float Angle)
+{
+	PWM_SETCompare2(Angle/180*2000+500);
+}
