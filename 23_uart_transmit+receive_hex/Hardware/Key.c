dae7f2cec74e05c2b59389491f1b8084fbc524b6
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2025-12-21 12:24:48 +0800

    23_uart_transmit+receive_hex

---
+++ 23_uart_transmit+receive_hex/Hardware/Key.c
@@ -0,0 +1,36 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+
+void Key_init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_1 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+
+}
+
+uint8_t Key_GetNum(void)//“按键数字获取”函数，返回值为无符号整形
+{
+	uint8_t KeyNum =0;//初始值为0
+    if (GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1) == 0)//按下1号按键
+	{
+		Delay_ms(200);//延迟消抖
+		while (GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1==0))//如不松手一直卡在这里
+		Delay_ms(200);
+		KeyNum=1;//返回值为1
+	}
+	
+	  if (GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_11) == 0)//按下11号按键
+	{
+		Delay_ms(200);//延迟消抖
+		while (GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_11==0))//如不松手一直卡在这里
+		Delay_ms(200);
+		KeyNum=2;//返回值为2
+	}
+	
+		return KeyNum;//得到返回值，默认为0，按1号按键为1，2号为2
+}
---
+++ 23_uart_transmit+receive_hex/Hardware/Serial.h
@@ -0,0 +1,24 @@
+#ifndef __SERIAL_H
+#define __SERIAL_H
+
+#include <stdio.h>
+
+extern uint8_t Serial_TxPacket[];
+extern uint8_t Serial_RxPacket[];
+
+
+void Serial_Init(void);
+void Serial_SendByte(uint8_t Byte);
+void Serial_SendArray(uint8_t *Array,uint16_t Length);
+void Serial_SendString(char *String);
+void Serial_SendNumber(uint32_t Number,uint16_t Length);
+void Serial_Printf(char *format,...);
+uint8_t Serial_GetRxFlag(void);
+void USART1_IRQHandler(void);
+void Serial_SendPacket(void);
+
+
+
+
+
+#endif
