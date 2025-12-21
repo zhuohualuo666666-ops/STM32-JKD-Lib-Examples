afd276393b5798df5f6f7b9eb37e267d6c1d992b
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2025-12-21 12:22:05 +0800

    22_uart_transmit+receive

---
+++ 22_uart_transmit+receive/User/main.c
@@ -0,0 +1,26 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Serial.h"
+
+uint8_t KeyNum;
+uint8_t RxData;
+
+int main(void)
+{
+	OLED_Init();
+	OLED_ShowString(1,1,"RxData:");
+	Serial_Init();
+	
+	
+	Serial_Printf("Num=%d\r\n,",666);
+	while(1)
+	{
+		if(Serial_GetRxFlag()==1)
+		{
+		RxData=Serial_GetRxData();
+		Serial_SendByte(RxData);
+		OLED_ShowHexNum(1,8,RxData,2);
+		}
+	}
+}
