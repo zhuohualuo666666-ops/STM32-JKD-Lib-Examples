cc50580bbf75cb9f07c15528be56bcc67a19edba
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2026-01-08 20:38:22 +0800

    24_uart transmit+receive_text
    串口收发文本数据包

---
+++ 24_uart transmit+receive_text/User/main.c
@@ -0,0 +1,54 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Serial.h"
+#include "LED.h"
+#include <string.h>
+
+
+
+int main(void)
+{
+	OLED_Init();
+	LED_init();
+	Serial_Init();
+	
+	OLED_ShowString(1,1,"TxPacket");
+	OLED_ShowString(3,1,"RxPacket");
+
+	
+	
+
+
+	while(1)
+	{
+		if(Serial_RxFlag==1)
+		{ 
+		OLED_ShowString(4,1,"                          ");
+		OLED_ShowString(4,1,Serial_RxPacket);
+			
+		if(strcmp(Serial_RxPacket,"LED_ON")	==0)
+		{
+			LED1_ON();
+			Serial_SendString("LED_ON_OK\r\n");
+			OLED_ShowString(2,1,"                        ");
+			OLED_ShowString(2,1,"LED_ON_OK\r\n");
+		}	
+		else if(strcmp(Serial_RxPacket,"LED_OFF")	==0)
+		{
+			LED1_OFF();
+			Serial_SendString("LED_OFF_OK\r\n");
+			OLED_ShowString(2,1,"                        ");
+			OLED_ShowString(2,1,"LED_OFF_OK\r\n");
+		}	
+		else
+		{
+			Serial_SendString("ERROR_COMMAND\r\n");
+			OLED_ShowString(2,1,"                        ");
+			OLED_ShowString(2,1,"ERROR_COMMAND\r\n");
+		}
+		
+		Serial_RxFlag =0;
+		}	
+	}
+}
