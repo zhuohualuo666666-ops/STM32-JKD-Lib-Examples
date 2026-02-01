6e5f09a3ef7f9b45bf333ed7f8891117792c80cf
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2026-02-01 17:43:49 +0800

    34_Standby + Real-time

---
+++ 34_Standby + Real-time/User/main.c
@@ -0,0 +1,45 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "MyRTC.h"
+
+int main(void)
+{
+	OLED_Init();
+	MyRTC_Init();
+	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+
+	
+	OLED_ShowString(1, 1, "CNT:");
+	OLED_ShowString(2, 1, "ALR:");
+	OLED_ShowString(3, 1, "ALRF:");
+	
+	PWR_WakeUpPinCmd(ENABLE);
+	
+	uint32_t alarm =RTC_GetCounter()+10;
+	RTC_SetAlarm(alarm);
+	OLED_ShowNum(2,6,alarm,10);
+	
+	while (1)
+	{
+		
+		
+		OLED_ShowNum(1, 6, RTC_GetCounter(), 10);
+		OLED_ShowNum(3, 6, RTC_GetFlagStatus(RTC_FLAG_ALR), 1);
+ 
+		OLED_ShowString(4,1,"running");
+		Delay_ms(100);  
+		OLED_ShowString(4,1,"       ");
+		Delay_ms(100);
+
+		OLED_ShowString(4,1,"standby");
+		Delay_ms(1000);  
+		OLED_ShowString(4,1,"       ");
+		Delay_ms(1000);
+		
+		OLED_Clear();
+		
+		PWR_EnterSTANDBYMode();
+	}
+}
